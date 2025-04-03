6e9b4abf55570f98afa50a23a5bb6673fc759043
Gustavo <gw79245@gmail.com>
2025-04-03 19:31:55 -0300

    começo do trabalho

---
+++ src/Rua.java
@@ -0,0 +1,46 @@
+public class Rua {
+    private int comprimento;
+    private int capacidade;
+    private int tempo_travessia;
+    private int horario_de_pico;
+
+    public Rua(int comprimento, int capacidade, int tempo_travessia, int horario_de_pico) {
+        this.comprimento = comprimento;
+        this.capacidade = capacidade;
+        this.tempo_travessia = tempo_travessia;
+        this.horario_de_pico = horario_de_pico;
+    }
+
+    public int getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(int comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public int getTempo_travessia() {
+        return tempo_travessia;
+    }
+
+    public void setTempo_travessia(int tempo_travessia) {
+        this.tempo_travessia = tempo_travessia;
+    }
+
+    public int getHorario_de_pico() {
+        return horario_de_pico;
+    }
+
+    public void setHorario_de_pico(int horario_de_pico) {
+        this.horario_de_pico = horario_de_pico;
+    }
+
+}
---
+++ src/Semaforo.java
@@ -0,0 +1,27 @@
+public class Semaforo {
+    private int tempo_ciclo;
+    private int tempo_verde;
+    private int tempo_amarelo;
+    private int tempo_vermelho;
+
+
+    public int getTempo_ciclo() {
+        return tempo_ciclo;
+    }
+
+    public void setTempo_ciclo(int tempo_ciclo) {
+        this.tempo_ciclo = tempo_ciclo;
+    }
+
+    public void cicloFixo(){
+
+    }
+
+    public void otimizarTempo(){
+
+    }
+
+    public void otimizarEnergia(){
+
+    }
+}
